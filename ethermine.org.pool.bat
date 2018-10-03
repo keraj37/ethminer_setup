f59be0c457f98e6e32ac36dfb40cad133e6ce00c
Jerry Switalski <jerry.switalski@gmail.com>
2018-10-03 18:28:02 +0200

    p

--- ethermine.org.pool.bat
+++ ethermine.org.pool.bat
@@ -3,4 +3,4 @@ setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
-ethminer.exe --farm-recheck 200 -U -S eu2.ethermine.org:4444 -FS eu2.ethermine.org:4444 -O 0xcf57d6328f36f4a48ff2e22cc17ecd8044bd526b.ANOSLAB2
+ethminer.exe --farm-recheck 200 -U -S eu2.ethermine.org:4444 -FS eu2.ethermine.org:4444 -O 0xcf57d6328f36f4a48ff2e22cc17ecd8044bd526b.main01
