e6a137b38704e6dc79291e2e31e87550ab0d6fcc
Jerry Switalski <jerry.switalski@gmail.com>
2018-10-04 15:58:53 +0200

    p

---
+++ etc/start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+finminer.exe -epool etc-eu1.nanopool.org:19999 -ewal 0xcf57d6328f36f4a48ff2e22cc17ecd8044bd526b.main_etc_01/j@quisutdeus.in -epsw x -mode 1 -ftime 10
--- newest/ethermine.org.pool.bat
+++ newest/ethermine.org.pool.bat
@@ -3,4 +3,4 @@ setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
-ethminer.exe --farm-recheck 200 -U -P stratum1+tcp://0xcf57d6328f36f4a48ff2e22cc17ecd8044bd526b.main01@eu2.ethermine.org:4444
+ethminer.exe --farm-recheck 200 -U -P stratum1+tcp://0xcf57d6328f36f4a48ff2e22cc17ecd8044bd526b.main02@eu1.ethpool.org:3333
